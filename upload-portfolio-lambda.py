3d184b057a21cc540d27db96d98f527e193ed09e
danocolombo@google.com <GR0mans1212!>
2020-05-09 20:55:51 -0400

    added the sns configuration to python script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,7 +5,11 @@ import mimetypes
 
 
 def lambda_handler(event, context):
-
+    sns = boto3.resource('sns')
+    topic = sns.Topic(
+        'arn:aws:sns:us-east-1:226036691416:deployForstonGuruTopic')
+    topic.publish(Subject="Fortson.Guru - redeployed",
+                  Message="The fortson.guru site has been updated with the latest code from Github")
     s3 = boto3.resource('s3')
     portfolio_bucket = s3.Bucket('fortson.guru')
     build_bucket = s3.Bucket('build.fortson.guru')
@@ -21,5 +25,7 @@ def lambda_handler(event, context):
             portfolio_bucket.upload_fileobj(
                 obj, nm,  ExtraArgs={'ContentType': str(mime_type)})
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-    return 'Hello from Lambda'
+    print "Job done!"
+    topic.publish(Subject="Fortson.Guru - redeployed",
+                  Message="The fortson.guru site has been updated with the latest code from Github")
+    return 'Hello from Lamb'
