7b0ef0ac84c7f99e8adf01e8d63b3e2ad652c8b2
danocolombo@google.com <GR0mans1212!>
2020-05-09 20:24:59 -0400

    actual lambda function tested and passed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,16 +2,24 @@ import boto3
 import io
 import zipfile
 import mimetypes
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('fortson.guru')
-build_bucket = s3.Bucket('build.fortson.guru')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('1563f35e-3114-4d95-a138-3402f5288249/buildFortsonGuru.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist(): 
-        obj = myzip.open(nm) 
-        mime_type = mimetypes.guess_type(nm)[0]  
-        portfolio_bucket.upload_fileobj(obj, nm,  ExtraArgs={'ContentType': str(mime_type)})  
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+def lambda_handler(event, context):
+
+    s3 = boto3.resource('s3')
+    portfolio_bucket = s3.Bucket('fortson.guru')
+    build_bucket = s3.Bucket('build.fortson.guru')
+
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj(
+        '1563f35e-3114-4d95-a138-3402f5288249/buildFortsonGuru.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            mime_type = mimetypes.guess_type(nm)[0]
+            portfolio_bucket.upload_fileobj(
+                obj, nm,  ExtraArgs={'ContentType': str(mime_type)})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Hello from Lambda'
